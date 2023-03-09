b3aa27ec6cc513ff1d75358d7614207b1a0e0565
Christopher <latechstudent17@gmail.com>
2023-03-09 10:00:31 -0600

    Updated scheduledtask

---
+++ Jellyfin.Plugin.Newsletters/ScheduledTasks/EmailNewsletter.cs
@@ -0,0 +1,72 @@
+#pragma warning disable CS1591
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Jellyfin.Plugin.Newsletters.Scripts;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Globalization;
+using MediaBrowser.Model.Tasks;
+
+namespace Jellyfin.Plugin.Newsletters.ScheduledTasks
+{
+    /// <summary>
+    /// Class RefreshMediaLibraryTask.
+    /// </summary>
+    public class EmailNewsletter : IScheduledTask
+    {
+        // public ILibraryManager _libraryManager;
+        // public ILocalizationManager _localization;
+
+        // /// <summary>
+        // /// Initializes a new instance of the <see cref="RefreshMediaLibraryTask" /> class.
+        // /// </summary>
+        // /// <param name="libraryManager">Instance of the <see cref="ILibraryManager"/> interface.</param>
+        // /// <param name="localization">Instance of the <see cref="ILocalizationManager"/> interface.</param>
+        // public void RefreshMediaLibraryTask(ILibraryManager libraryManager, ILocalizationManager localization)
+        // {
+        //     _libraryManager = libraryManager;
+        //     _localization = localization;
+        // }
+
+        /// <inheritdoc />
+        public string Name => "Newsletter Scheduler";
+
+        /// <inheritdoc />
+        public string Description => "Email Newsletters";
+
+        /// <inheritdoc />
+        public string Category => "Newsletters";
+
+        /// <inheritdoc />
+        public string Key => "EmailNewsletters";
+
+        /// <summary>
+        /// Creates the triggers that define when the task will run.
+        /// </summary>
+        /// <returns>IEnumerable{BaseTaskTrigger}.</returns>
+        public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+        {
+            yield return new TaskTriggerInfo
+            {
+                Type = TaskTriggerInfo.TriggerInterval,
+                IntervalTicks = TimeSpan.FromHours(12).Ticks
+            };
+        }
+
+        /// <inheritdoc />
+        public Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            progress.Report(0);
+            Console.WriteLine("I made it!!!");
+            // return ((LibraryManager)_libraryManager).ValidateMediaLibraryInternal(progress, cancellationToken);
+            string to = "christopher.hebert94@gmail.com";
+            string from = "donotreply";
+            Smtp.SendSmtp(to, from);
+            return Task.CompletedTask;
+        }
+    }
+}
--- Jellyfin.Plugin.Newsletters/Scripts/smtp.cs
+++ Jellyfin.Plugin.Newsletters/Scripts/smtp.cs
@@ -1,11 +1,17 @@
+#pragma warning disable 1591
 using System;
 using System.Net;
 using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Globalization;
+using MediaBrowser.Model.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using static System.Net.NetworkCredential;
 
-namespace Jellyfin.Plugin.NewsLetters.Scripts
+namespace Jellyfin.Plugin.Newsletters.Scripts
 {
     /// <summary>
     /// Interaction logic for SendMail.xaml.
@@ -18,32 +24,48 @@ namespace Jellyfin.Plugin.NewsLetters.Scripts
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        private void SendSmtp(string recipient, string sender)
+        public static void SendSmtp(string recipient, string sender)
         {
             // created object of SmtpClient details and provides server details
             System.Console.WriteLine("Running SMTP C# File");
             SmtpClient smtp = new SmtpClient();
-            smtp.Host = string.Empty;
-            smtp.Port = 81;
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
 
             // Server Credentials
             NetworkCredential nc = new NetworkCredential();
-            nc.UserName = string.Empty;
-            nc.Password = string.Empty;
+            nc.UserName = "chris.hebert.jesusfreak09@gmail.com";
+            nc.Password = "hmctnexjirhhphms";
             // assigned credetial details to server
             smtp.Credentials = nc;
 
             // create sender address
-            MailAddress from = new MailAddress("Sender Address", "Name want to display");
+            MailAddress from = new MailAddress(sender, sender);
 
             // create receiver address
-            MailAddress receiver = new MailAddress(recipient, "Name want to display");
+            MailAddress receiver = new MailAddress(recipient, recipient);
 
             MailMessage mymessage = new MailMessage(from, receiver);
-            mymessage.Subject = string.Empty;
+            mymessage.Subject = "this is a test message";
             mymessage.Body = string.Empty;
             // sends the email
             smtp.Send(mymessage);
         }
+
+/*
+        public Task SendSmtpTask(IProgress<double> progress, CancellationToken cancellationToken)
+        {
+            try {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                Console.WriteLine("I made it!!!");
+                progress.Report(100);
+            }
+            catch (Exception e) {
+                Console.WriteLine("{0} Exception Caught.", e);
+            }
+            return Task.CompletedTask;
+        }
+*/
     }
 }
