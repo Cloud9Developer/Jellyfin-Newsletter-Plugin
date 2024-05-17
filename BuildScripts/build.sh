9bf600f1ebb437ac8d236e4d019bd1d86aaaa2f0
christopher <christopher.hebert94@gmail.com>
2024-05-17 11:52:40 -0500

    updated build scripts for ease-of-life

--- BuildScripts/build.sh
+++ BuildScripts/build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 TAG=${1}
-if ! docker images -a | grep mcr.microsoft.com/dotnet/sdk${TAG}.0; then
-    echo "Pulling dotnet:${TAG}.0 image"
-    docker pull mcr.microsoft.com/dotnet/sdk:${TAG}.0
+
+#### V1 ####
+
+# if ! docker images -a | grep mcr.microsoft.com/dotnet/sdk${TAG}.0; then
+#     echo "Pulling dotnet:${TAG}.0 image"
+#     docker pull mcr.microsoft.com/dotnet/sdk:${TAG}.0
+# else
+#     echo "Image already exists!"
+# fi
+
+# docker run --name dotnet -it -v .:/Development mcr.microsoft.com/dotnet/sdk:${TAG}.0 /Development/BuildScripts/dotnetbuild.sh
+# docker rm dotnet
+
+#### V2 ####
+IMAGE="pypi_dotnet"
+echo "Using image: '${IMAGE}:${TAG}.0'"
+if ! docker images -a | grep "${IMAGE}" | grep "${TAG}.0"; then
+    echo "Generating '${IMAGE}:${TAG}.0' image"
+    docker build -t ${IMAGE}:${TAG}.0 --build-arg IMAGE_TAG=8 ./BuildScripts/
 else
     echo "Image already exists!"
 fi
 
-docker run --name dotnet -it -v .:/Development mcr.microsoft.com/dotnet/sdk:${TAG}.0 /Development/BuildScripts/dotnetbuild.sh
+docker run --name dotnet -it -v .:/Development ${IMAGE}:${TAG}.0 /Development/BuildScripts/dotnetbuild.sh "${2}"
 docker rm dotnet
+
+# docker build -t pypi_dotnet:8.0 --build-arg IMAGE_TAG=8 .
